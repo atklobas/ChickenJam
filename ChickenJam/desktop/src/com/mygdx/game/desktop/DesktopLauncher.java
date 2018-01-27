379ec216d0859d462a1d4889aa181d776543116f
Jeff Talada <j.talada@bellevuecollege.edu>
2018-01-26 18:29:17 -0800

    Everything changed

--- ChickenJam/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ ChickenJam/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.ChickenGame;
+import com.mygdx.game.JamChicken;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new ChickenGame(), config);
+		new LwjglApplication(new JamChicken(), config);
 	}
 }
